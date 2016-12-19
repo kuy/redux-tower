a6174a2d2fea845782585852267de68a689e0800
Yuki Kodama <endflow.net@gmail.com>
2016-12-20 02:44:11 +0900

    Enable production build

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,9 @@ module.exports = {
     publicPath: '/in-memory'
   },
   plugins: (process.env.NODE_ENV === 'production') ? [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
     })
